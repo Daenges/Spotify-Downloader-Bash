de430620c0f9658fdea85fa9beb2cb748628bd6e
Daenges <Daenges@proton.me>
2025-02-22 00:03:43 +0100

    Add Lyrics imbedding to script

--- Downloader.sh
+++ Downloader.sh
@@ -8,7 +8,7 @@ additionalKeywords=""
 
 ###
 # Set operation variables
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "ERROR: No .csv file provided. Obtain one here: https://watsonbox.github.io/exportify/"
     exit 1
 else
@@ -18,12 +18,12 @@ fi
 while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
-        declare $param="$2"
+        declare "$param"="$2"
    fi
    shift
 done
 
-if [[ ! -z $musicPath ]] && [[ ! -d "$musicPath" ]]
+if [[ -n $musicPath ]] && [[ ! -d "$musicPath" ]]
 then
     echo "ERROR: Selected path does not exist: $musicPath"
     exit 1
@@ -63,23 +63,69 @@ colNameAlbumName="Album Name"
 colNameAlbumArtistName="Album Artist Name(s)"
 colNameDiscNum="Disc Number"
 colNameTrackNumber="Track Number"
+colNamePopularity="Popularity"
+colNameAlbumReleaseDate="Album Release Date"
+colNameDuration="Track Duration (ms)"
 ###
 
-csvHeader=$(head -1 $csvFile | tr ',' '\n' | nl)
+csvHeader=$(head -1 "$csvFile" | tr ',' '\n' | nl)
 
 ###
 # Evaluate the coresponding column number for each name
 colNumTitle=$(echo "$csvHeader" | grep -w "$colNameTitle" | tr -d " " | awk -F " " '{print $1}')
 colNumArtist=$(echo "$csvHeader" | grep -w "$colNameArtist" | tr -d " " | awk -F " " '{print $1}' | head -1)
-colNumImageURL=$(echo "$csvHeader" | grep -w "$colNameImageURL" | tr -d " " | awk -F " " '{print $1}')
 colNumAlbumName=$(echo "$csvHeader"  | grep -w "$colNameAlbumName" | tr -d " " | awk -F " " '{print $1}')
 colNumAlbumArtistName=$(echo "$csvHeader" | grep -w "$colNameAlbumArtistName" | tr -d " " | awk -F " " '{print $1}')
+colNumAlbumReleaseDate=$(echo "$csvHeader" | grep -w "$colNameAlbumReleaseDate" | tr -d " " | awk -F " " '{print $1}')
+colNumImageURL=$(echo "$csvHeader" | grep -w "$colNameImageURL" | tr -d " " | awk -F " " '{print $1}')
 colNumDiscNumber=$(echo "$csvHeader" | grep -w "$colNameDiscNum" | tr -d " " | awk -F " " '{print $1}')
 colNumTrack=$(echo "$csvHeader" | grep -w "$colNameTrackNumber" | tr -d " " | awk -F " " '{print $1}')
+colNumDuration=$(echo "$csvHeader" | grep -w "$colNameDuration" | tr -d " " | awk -F " " '{print $1}')
+colNumPopularity=$(echo "$csvHeader" | grep -w "$colNamePopularity" | tr -d " " | awk -F " " '{print $1}')
 ###
 
 ###
 # Declare functions
+
+# Function to get lyrics
+downloadLrc() {
+    # Input parameters
+    track_name="$1"
+    artist_name="$2"
+    album_name="$3"
+    duration_ms="$4"
+
+    # Convert duration from milliseconds to seconds (integer division)
+    duration=$((duration_ms / 1000))
+
+    # URL encode the input parameters (if needed)
+    encoded_track_name=$(echo "$track_name" | jq -sRr @uri)
+    encoded_artist_name=$(echo "$artist_name" | jq -sRr @uri)
+    encoded_album_name=$(echo "$album_name" | jq -sRr @uri)
+
+    # API URL
+    url="https://lrclib.net/api/get?track_name=${encoded_track_name}&artist_name=${encoded_artist_name}&album_name=${encoded_album_name}&duration=${duration}"
+
+    # Request the lyrics from the API
+    response=$(curl -A "SDB v0.0.1 (https://github.com/Daenges/Spotify-Downloader-Bash)" -s -w "%{http_code}" "$url")
+
+    # Check the HTTP status code
+    http_code="${response: -3}"
+    response_body="${response:0:${#response}-3}"
+
+    if [[ "$http_code" -eq 200 ]]; then
+        # Extract synchronized lyrics or fallback to plain lyrics
+        synced_lyrics=$(jq -r '.syncedLyrics // empty' <<< "$response_body")
+
+        # If synced lyrics exist, output them; otherwise, fallback to plain lyrics
+        if [[ -n "$synced_lyrics" ]]; then
+            echo "$synced_lyrics" > "/tmp/${track_name}.lrc"
+        else
+            plain_lyrics=$(jq -r '.plainLyrics' <<< "$response_body")
+            echo "$plain_lyrics" > "/tmp/${track_name}.lrc"
+        fi
+    fi
+}
 
 # Escape special characters for urls: https://gist.github.com/cdown/1163649
 urlencode() {
@@ -109,6 +155,10 @@ clearCache() {
     if [[ -f "/tmp/$1.jpg" ]]; then
         rm "/tmp/$1.jpg"
     fi
+
+    if [[ -f "/tmp/$1.lrc" ]]; then
+        rm "/tmp/$1.lrc"
+    fi
 }
 ###
 
@@ -119,10 +169,10 @@ songArray=()
 
 ###
 # Populate array from file
-while read songLine
+while read -r songLine
 do
     songArray+=("$songLine")
-done < <(tail -n +2 $csvFile)
+done < <(tail -n +2 "$csvFile")
 echo "Found: ${#songArray[@]} entries"
 ###
 
@@ -131,19 +181,22 @@ echo "Found: ${#songArray[@]} entries"
 crawlerTask() {
     ###
     # Split column names to array
-    colArray=($(echo "$1" | grep -Eo '"([^"]*)"' | sed 's/\"//g' | tr ' ' '_'))
+    colArray=($(echo "$1" | sed 's/\"\"/\"_\"/g' | grep -Eo '"([^"]*)"' | sed 's/\"//g' | tr ' ' '_'))
     ###
 
     ###
     # Assert values for this entry to variables
     # Special replacement for songTitle as it is used for paths
-    songTitle="$(echo ${colArray[$colNumTitle - 1]} | tr '_/\\' ' ')"
-    artist="$(echo ${colArray[$colNumArtist - 1]} | tr '_' ' ' | cut -f1 -d',')"
-    image="$(echo ${colArray[$colNumImageURL - 1]} | tr '_' ' ')"
-    albumName="$(echo ${colArray[$colNumAlbumName - 1]} | tr '_' ' ')"
-    albumArtistsName="$(echo ${colArray[$colNumAlbumArtistName - 1]} | tr '_' ' ')"
-    discNumber="$(echo ${colArray[$colNumDiscNumber - 1]} | tr '_' ' ')"
-    trackNumber="$(echo ${colArray[$colNumTrack - 1]}| tr '_' ' ')"
+    songTitle="$(echo "${colArray[$colNumTitle - 1]}" | tr '_/\\' ' ')"
+    artist="$(echo "${colArray[$colNumArtist - 1]}" | tr '_' ' ' | cut -f1 -d',')"
+    albumName="$(echo "${colArray[$colNumAlbumName - 1]}" | tr '_' ' ')"
+    albumArtistsName="$(echo "${colArray[$colNumAlbumArtistName - 1]}" | tr '_' ' ')"
+    albumReleaseDate="$(echo "${colArray[$colNumAlbumReleaseDate - 1]}" | tr '_' ' ')"
+    image="$(echo "${colArray[$colNumImageURL - 1]}" | tr '_' ' ')"
+    discNumber="$(echo "${colArray[$colNumDiscNumber - 1]}" | tr '_' ' ')"
+    trackNumber="$(echo "${colArray[$colNumTrack - 1]}" | tr '_' ' ')"
+    trackDuration="$(echo "${colArray[$colNumDuration - 1]}" | tr '_' ' ')"
+    popularityScore="$(echo "${colArray[$colNumPopularity - 1]}" | tr '_' ' ')"
     ###
 
     # Remove possible old cache
@@ -158,25 +211,55 @@ crawlerTask() {
 
         ###
         # Get cover and .mp3 file
-        curl -s $image > "/tmp/${songTitle}.jpg" &
-        $downloader -o "/tmp/${songTitle}.%(ext)s" $songURL -I 1 -x --audio-format mp3 --audio-quality 0 --quiet &
+        curl -s "$image" > "/tmp/${songTitle}.jpg" &
+        $downloader -o "/tmp/${songTitle}.%(ext)s" "$songURL" -I 1 -x --audio-format mp3 --audio-quality 0 --quiet &
         wait
         ###
 
-        ###
-        # Merge cover, metadata and .mp3 file
-        ffmpeg -i "/tmp/${songTitle}.mp3" -i "/tmp/$songTitle.jpg" \
-        -map 0:0 -map 1:0 -codec copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" \
-        -metadata artist="$artist" \
-        -metadata album="$albumName" \
-        -metadata album_artist="$albumArtistsName" \
-        -metadata disc="$discNumber" \
-        -metadata title="$songTitle" \
-        -metadata track="$trackNumber" \
-        -hide_banner \
-        -loglevel error \
-        "${musicPath}${songTitle}.mp3" -y
-        ###
+        ### Imbed Metadata
+        if command -v eyeD3 &> /dev/null && command -v jq &> /dev/null; then
+            # Get the Lyrics from https://lrclib.net/
+            downloadLrc "${songTitle}" \
+                        "${albumArtistsName}" \
+                        "${albumName}" \
+                        "${trackDuration}"
+
+            # Use eyeD3
+            eyeD3 --v2 \
+            --title "${songTitle}" \
+            --artist "${albumArtistsName}" \
+            --album "${albumName}" \
+            --release-date "${albumReleaseDate}" \
+            --disc-num "${discNumber}" \
+            --track "${trackNumber}" \
+            --add-popularity "support@spotify.com:${popularityScore}" \
+            "/tmp/${songTitle}.mp3" # &> /dev/null
+
+            if [[ -f "/tmp/${songTitle}.lrc" ]]; then
+                eyeD3 --v2 --add-lyrics "/tmp/${songTitle}.lrc" "/tmp/${songTitle}.mp3" # &> /dev/null
+            fi
+
+            if [[ -f "/tmp/${songTitle}.jpg" ]]; then
+                eyeD3 --v2 --add-image "/tmp/${songTitle}.jpg:FRONT_COVER" "/tmp/${songTitle}.mp3" # &> /dev/null
+            fi
+
+            mv "/tmp/${songTitle}.mp3" "${musicPath}${songTitle}.mp3"
+        else
+            ###
+            # Merge cover, metadata and .mp3 file
+            ffmpeg -i "/tmp/${songTitle}.mp3" -i "/tmp/$songTitle.jpg" \
+            -map 0:0 -map 1:0 -codec copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" \
+            -metadata artist="$artist" \
+            -metadata album="$albumName" \
+            -metadata album_artist="$albumArtistsName" \
+            -metadata disc="$discNumber" \
+            -metadata title="$songTitle" \
+            -metadata track="$trackNumber" \
+            -hide_banner \
+            -loglevel error \
+            "${musicPath}${songTitle}.mp3" -y
+            ###
+        fi
 
         ###
         # Clear cached files
@@ -199,11 +282,13 @@ numJobs="\j" # Number of background jobs.
 # Start parallel crawling instances
 for song in "${songArray[@]}"
 do
-    while (( ${numJobs@P} >= ${processNumber} )); do
+    while (( ${numJobs@P} >= processNumber )); do
         wait -n
     done
 
-    crawlerTask "$song" &
+    if [ -n "$song" ]; then
+        crawlerTask "$song" &
+    fi
 
     ((startedSongs++))
     echo "Status: #${startedSongs} downloads started - $(awk "BEGIN {print ((${startedSongs}/${#songArray[@]})*100)}")%"
